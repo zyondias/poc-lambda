3f64cf08692e86024ef8aa410b1dcefc98fef6fa
Zyon Dias <zyon.dias@hotmail.com>
2015-12-05 12:52:05 -0200

    removendo target

--- src/main/java/zyondev/lambda/PocLambda.java
+++ src/main/java/zyondev/lambda/PocLambda.java
@@ -1,5 +1,10 @@
 package zyondev.lambda;
 
+import com.amazonaws.services.lambda.runtime.Context;
+
 public class PocLambda {
 
+	 public String myHandler(int myCount, Context context) {
+	        return String.valueOf(myCount);
+	    }
 }
